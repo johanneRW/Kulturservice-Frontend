733f56e45fd100fa5be8d12332c32dae28a0d349
johanne <johanne_riis@hotmail.com>
2022-11-01 23:27:26 +0100

    Frontend til kulturservice

--- kulturService.js
+++ kulturService.js
@@ -64,43 +64,52 @@ function login(event) {
         })
 }
 
-// function getSecret(event) {
-//     const div = document.getElementById('container');
-//     event.preventDefault()
-//     if(localStorage.getItem('user') == undefined){
-//         div.insertAdjacentHTML(
-//             'beforeend',
-//             `<span style="background-color: red"> No token. Log in first.</code>`,
-//         );
-//         return;
-//     }
-//     var paramField = document.getElementById("paramField").value;
-//     var payload = {
-//         paramField: paramField
-//     };
-//     const localstorage_user = JSON.parse(localStorage.getItem('user'))
-//     const inMemoryToken = localstorage_user.token
-//     fetch("http://localhost:8080/getSecret", //"https://gaprepservice.azurewebsites.net/createUser"
-//         {
-//             method: "POST",
-//             body: payload,
-//             headers:{'content-type': 'application/json',
-//                 'Authorization': 'Bearer ' + inMemoryToken }
-//         })
-//         .then(function (res) { // the response to the fetch call itself
-//             div.insertAdjacentHTML(
-//                 'beforeend',
-//                 `<span style="background-color: red">` + res.status + `</code>`,
-//             );
-//             return res.json();
-//         })
-//         .then(function (data) { // the data, provided by server response
-//             div.insertAdjacentHTML(
-//                 'beforeend',
-//                 `<span style="background-color: green">` + JSON.stringify(data) + `</code>`,
-//             );
-//         })
-// }
+function getFutureEvents(event) {
+    const div = document.getElementById('container');
+    event.preventDefault()
+    if (localStorage.getItem('user') == undefined) {
+        div.insertAdjacentHTML(
+            'beforeend',
+            `<span style="background-color: red"> No token. Log in first.</code>`,
+        );
+        return;
+    }
+
+    const localstorage_user = JSON.parse(localStorage.getItem('user'))
+    const inMemoryToken = localstorage_user.token
+    fetch("http://localhost:8080/getFutureEvents",
+        {
+            method: "GET",
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': 'Bearer ' + inMemoryToken
+            }
+        })
+        .then(function (res) { // the response to the fetch call itself
+            if (res.status != 200) {
+                div.insertAdjacentHTML(
+                    'beforeend',
+                    `<span style="background-color: red">` + res.status + `</code>`,
+                );
+            }
+            return res.json();
+        })
+        .then(function (data) { // the data, provided by server response
+            if (data == null || data.length==0) {
+                div.insertAdjacentHTML(
+                    'beforeend',
+                    `<span style="background-color: yellow"> Fandt ingen kommende events</code>`,
+                );
+            } else {
+                data.forEach(event => {
+                    
+                div.insertAdjacentHTML(
+                    'beforeend',
+                    `<h4>`+event.bandName +'</h4> <h5>'+event.venue+': '+event.eventDate+'</h5>',
+                );
+            })}
+        })
+}
 
 function logout(event) {
     event.preventDefault()
